bd822a45e5331fa6f956e92e7bebda7e412b013a
4sunshine <alexandershain.5@gmail.com>
2020-07-25 20:14:27 +0300

    Numpy save implemented

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
-from magnetic.sav2vtk import convert_folder
+from magnetic.sav2vtk import convert_folder, box2curl2vtk, folder_derivative, box2npy
 
 
 if __name__ == '__main__':
-    folder = 'C:/AppsData/NLFFFE'
-    convert_folder(folder, 'B nlfffe')
+    folder = 'C:/AppsData/gx_out'
+    # folder_derivative(folder, 'dB', filter='.vtk')
+    convert_folder(folder, 'Bpot', func=box2npy)
 
--- magnetic/sav2vtk.py
+++ magnetic/sav2vtk.py
@@ -8,11 +8,8 @@ from tqdm import tqdm
 from magnetic.mathops import curl, angles, directions_closure
 
 
-def save_scalar_data(vx, vy, vz, vector_name, filename):
-    """NEED TO RE-IMPLEMENT THIS FUNCTION"""
-    assert np.shape(vx) == np.shape(vy) == np.shape(vz)
-    imageToVTK(filename, cellData={f'{vector_name} x': vx, f'{vector_name} y': vy,
-               f'{vector_name} z': vz})
+def save_scalar_data(s, scalar_name, filename):
+    imageToVTK(filename, pointData={scalar_name: s})
     return None
 
 
@@ -34,6 +31,37 @@ def field_from_box(filename):
     return bx, by, bz
 
 
+def energy_density(bx, by, bz):
+    e = np.power(bx, 2) + np.power(by, 2) + np.power(bz, 2)
+    return e / 8. / np.pi
+
+
+def free_energy(e_high, e_low, absolute=True):
+    if absolute:
+        return e_high - e_low
+    else:
+        return (e_high - e_low) / e_low
+
+
+def central_derivative(files, timestep):
+    reader = vtkStructuredPointsReader()
+    reader.SetFileName(files[0])
+    mesh = np.array(meshio.read(files[1]).point_data['Bnlfffe'])
+    print(np.shape(mesh))
+    return None
+
+
+def folder_derivative(path, field_name, func=free_energy, filter='.vtr', n_jobs=8, timestep=720):
+    """TIME DERIVATIVE OF DATA IN FOLDER. ASSUMING EQUIDISTANT FILES"""
+    """
+        PARAMETERS: timestep in seconds
+    """
+    all_files = sorted(files_list(path, filter))
+    files_triplets = [all_files[i-1: i+2] for i in range(1, len(all_files) - 1)]
+    central_derivative(files_triplets[0], timestep)
+    return None
+
+
 def prepare_target_name(filename, target_dir='target'):
     cd = os.path.dirname(filename)
     basename = os.path.basename(filename)
@@ -52,7 +80,21 @@ def box2vtk(filename, field_name):
     return None
 
 
+def box2npy(filename, field_name):
+    target_dir, basename = prepare_target_name(filename, target_dir='npy_field')
+    vx, vy, vz = field_from_box(filename)
+    basename += '.npy'
+    basename = field_name + '_' + basename
+    vx = np.expand_dims(vx, axis=-1)
+    vy = np.expand_dims(vy, axis=-1)
+    vz = np.expand_dims(vz, axis=-1)
+    v = np.concatenate([vx, vy, vz], axis=-1)
+    np.save(os.path.join(target_dir, basename), v)
+    return None
+
+
 def box2curl2vtk(filename, field_name):
+    """CONVERTS GX BOX TO VTK CURL"""
     target_dir, basename = prepare_target_name(filename, target_dir='vtk_curl')
     vx, vy, vz = field_from_box(filename)
     cx, cy, cz = curl(vx, vy, vz)
@@ -62,11 +104,17 @@ def box2curl2vtk(filename, field_name):
     return None
 
 
-def convert_folder(path, field_name, filter='.sav', n_jobs=8):
-    print(f'Start converting folder: {path}')
-    files = glob.glob(f'{path}/*{filter}')
-    print(f'Found {len(files)} files')
-    Parallel(n_jobs=n_jobs)(delayed(box2curl2vtk)(file, field)
+def convert_folder(path, field_name, func=box2vtk, filter='.sav', n_jobs=8):
+    """YOU SHOULD SPECIFY WHAT FUNCTION TO USE IN PARALLEL"""
+    files = files_list(path, filter)
+    Parallel(n_jobs=n_jobs)(delayed(func)(file, field)
                             for file, field in tqdm(zip(files, [field_name]*len(files))))
     return None
+
+
+def files_list(path, filter):
+    print(f'Start searching for files *{filter} in folder: {path}')
+    files = glob.glob(f'{path}/*{filter}')
+    print(f'Found {len(files)} files')
+    return files
 

e69cb1c010d4db6233fb7927f4f13c2819567603
4sunshine <alexandershain.5@gmail.com>
2020-08-02 19:00:34 +0300

    before_pulling

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
 from magnetic.sav2vtk import convert_folder
+import vtk
 
 
 if __name__ == '__main__':
-    folder = 'C:/AppsData/NLFFFE'
-    convert_folder(folder, 'B nlfffe')
+    # folder = 'C:/AppsData/NLFFFE'
+    # convert_folder(folder, 'B nlfffe')
+    print('ok')
+
 
